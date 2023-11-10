35c9479096e0a7fa1df1655be3855f1a78da5c6a
Karthi0505 <karthi.uideveloper@gmail.com>
2023-11-10 18:35:22 +0530

    Array Practice pages  added

---
+++ students/js/students.js
@@ -0,0 +1,141 @@
+const students = [
+    {
+        name: "Sandhiya",
+        batch : 2,
+        pages: [
+            {
+                pageHeading: "Array Practice",
+                url: "Sandhiya/practice_array.html",
+            }
+        ]
+    },
+    {
+        name: "Keerthika K",
+        batch : 2,
+        pages: [
+            {
+                pageHeading: "Array Practice",
+                url: "Keerthika_K/practice_array.html",
+            }
+        ]
+    },
+    {
+        name: "Ajay",
+        batch : 2,
+        pages: [
+            {
+                pageHeading: "Array Practice",
+                url: "Ajay/practice_array.html",
+            }
+        ]
+    },
+    {
+        name: "Kiruthika S",
+        batch : 2,
+        pages: [
+            {
+                pageHeading: "Array Practice",
+                url: "Kiruthika_S/practice_array.html",
+            }
+        ]
+    },
+    {
+        name: "Praveen",
+        batch : 2,
+        pages: [
+            {
+                pageHeading: "Array Practice",
+                url: "Praveen/practice_array.html",
+            }
+        ]
+    },
+    {
+        name: "Saratha",
+        batch : 2,
+        pages: [
+            {
+                pageHeading: "Array Practice",
+                url: "Kiruthika_S/practice_array.html",
+            }
+        ]
+    },
+    {
+        name: "Lakshmi",
+        batch : 2,
+        pages: [
+            {
+                pageHeading: "Array Practice",
+                url: "Kiruthika_S/practice_array.html",
+            }
+        ]
+    },
+    {
+        name: "Arshad",
+        batch : 2,
+        pages: [
+            {
+                pageHeading: "Array Practice",
+                url: "Kiruthika_S/practice_array.html",
+            }
+        ]
+    },
+    {
+        name: "Rathna",
+        batch : 2,
+        pages: [
+            {
+                pageHeading: "Array Practice",
+                url: "Kiruthika_S/practice_array.html",
+            }
+        ]
+    },
+    {
+        name: "Nivethitha",
+        batch : 2,
+        pages: [
+            {
+                pageHeading: "Array Practice",
+                url: "Kiruthika_S/practice_array.html",
+            }
+        ]
+    },
+    {
+        name: "Logesh",
+        batch : 2,
+        pages: [
+            {
+                pageHeading: "Array Practice",
+                url: "Kiruthika_S/practice_array.html",
+            }
+        ]
+    },
+    {
+        name: "Selvarasu",
+        batch : 2,
+        pages: [
+            {
+                pageHeading: "Array Practice",
+                url: "Kiruthika_S/practice_array.html",
+            }
+        ]
+    },
+    {
+        name: "Karthi",
+        batch : 2,
+        pages: [
+            {
+                pageHeading: "Array Practice",
+                url: "Karthi/practice_array.html",
+            }
+        ]
+    }
+];
+
+
+
+(function(){ //IIFE Start
+
+   
+
+
+})() //IIFE End
