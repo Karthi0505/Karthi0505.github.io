3c7ce4361a02f92a7e57899d01f49f0371fb27b7
Karthi0505 <karthi.uideveloper@gmail.com>
2023-12-01 20:39:22 +0530

    Fix in PDF

--- js/custom.js
+++ js/custom.js
@@ -1,16 +1,19 @@
 (function() { //IIFE Start
 
 
+
 // pdf to html Start ###############################################
 function fn_save_pdf() {
     var source = document.getElementById('body');
-    source.classList.add('print');
+    source.classList.add('print'); //add this class before conversion starts, and do PDF necessary CSS with this class
 
     var element = document.getElementById('element-to-print');
+    //Heading to keep as filename
+    let title = document.querySelector('h1').innerText.replaceAll(" ", "_"); 
     var opt = {
       pagebreak: {mode: ['avoid-all', 'css', 'legacy']},
       margin: 0,
-      filename: 'myfile.pdf',
+      filename: title+'.pdf',
       image: { type: 'jpeg', quality: 0.98 },
       html2canvas: { scale: 2 },
       jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' }
