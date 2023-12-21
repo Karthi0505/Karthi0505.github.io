20f3bd7fdd1fb572bdf6496e9d96840a1b9e5dca
Karthi0505 <karthi.uideveloper@gmail.com>
2023-12-21 15:33:19 +0530

    added go to top and new normal print js implemented

--- js/custom.js
+++ js/custom.js
@@ -1,19 +1,53 @@
-(function() { //IIFE Start
+(function () { //IIFE Start
 
 
+  // Print Js Start
+  const printButtons = document.querySelectorAll('.print-JS');
 
-// pdf to html Start ###############################################
-function fn_save_pdf() {
+  printButtons.length > 0 && (
+
+    printButtons.forEach((button) => {
+      button.addEventListener('click', () => {
+        window.print();
+      })
+    })
+
+  ) // && condition end
+  // Print Js End
+
+
+  // Scroll to top - Start #####################################################
+  let scrollTopBtn = document.getElementById("scrollTopBtn");
+
+  window.onscroll = function () {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+      scrollTopBtn.style.display = "block";
+    } else {
+      scrollTopBtn.style.display = "none";
+    }
+  };
+
+  scrollTopBtn.onclick = function () {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  };
+  // Scroll to top - End -------------------------------------------------------
+
+
+  // pdf to html Start ###############################################
+  const export_to_pdf_Button = document.getElementById('export_to_pdf_Button');
+
+  function fn_save_pdf() {
     var source = document.getElementById('body');
     source.classList.add('print'); //add this class before conversion starts, and do PDF necessary CSS with this class
 
     var element = document.getElementById('element-to-print');
     //Heading to keep as filename
-    let title = document.querySelector('h1').innerText.replaceAll(" ", "_"); 
+    let title = document.querySelector('h1').innerText.replaceAll(" ", "_");
     var opt = {
-      pagebreak: {mode: ['avoid-all', 'css', 'legacy']},
+      pagebreak: { mode: ['avoid-all', 'css', 'legacy'] },
       margin: 0,
-      filename: title+'.pdf',
+      filename: title + '.pdf',
       image: { type: 'jpeg', quality: 0.98 },
       html2canvas: { scale: 2 },
       jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' }
@@ -21,14 +55,16 @@ function fn_save_pdf() {
 
     // New Promise-based usage:
     html2pdf().set(opt).from(element).save()
-        //https://lightrun.com/answers/ekoopmans-html2pdf-js-media-print-or-media-not-print-in-css-ignored
-        .then(function(){ 
-            source.classList.remove('print');
-        });
-}
+      //https://lightrun.com/answers/ekoopmans-html2pdf-js-media-print-or-media-not-print-in-css-ignored
+      .then(function () {
+        source.classList.remove('print');
+      });
+  }
 
-document.getElementById('export_to_pdf_Button').addEventListener('click', fn_save_pdf)
-// pdf to html End -------------------------------------------------------
+  if (export_to_pdf_Button) {
+    document.getElementById('export_to_pdf_Button').addEventListener('click', fn_save_pdf)
+  }
+  // pdf to html End -------------------------------------------------------
 
 
 })() //IIFE End
