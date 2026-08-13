335389710526172c8f205225e3c1a4d9e1250d24
Karthi0505 <karthi.uideveloper@gmail.com>
2026-08-13 23:48:41 +0530

    typescript fix

---
+++ src/global.d.ts
@@ -0,0 +1,10 @@
+declare module '*.module.css';
+declare module '*.module.scss';
+declare module '*.css';
+declare module '*.scss';
+declare module '*.less';
+declare module '*.svg';
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.gif';
